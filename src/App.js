5fb541b475d1fd2f1219cb0ad3109ddaa7b2573e
daniyal8 <daniyalsadiq4@gmail.com>
2021-04-10 19:59:46 +0500

    feat: project-4b

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Brain from './Intro_Brain.svg'
+import Img from './Background.svg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="animation">
+      <img className="brain" src={Brain} />
+      <img className="Img" src={Img} />
+      </div>
     </div>
   );
 }
